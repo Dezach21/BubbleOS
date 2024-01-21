0b3b644a1bc8f6f5f66093affc2e8cb3c5792d29
Dezach21 <dezach2121@gmail.com>
2024-01-20 19:21:06 -0500

    Modifications.

--- main.asm
+++ main.asm
@@ -1,5 +1,5 @@
-org 0x7C00
-bits 16
+;org 0x7C00
+;bits 16
 
 infiniteLoop: ; Infinite loop
     jmp infiniteLoop
@@ -7,3 +7,4 @@ infiniteLoop: ; Infinite loop
 times 510-($-$$) db 0 ; Pad binary file with zeroes to reach 510 bytes
 
 dw 0xAA55 ; The last 2 bytes are the magic number for the BIOS to interpret this sector as a boot sector
+;times 1024-($-$$) db 0

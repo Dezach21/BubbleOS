fc392eb8cc6e2ddebd08224265a527f0738fe035
Dezach21 <dezach2121@gmail.com>
2024-01-20 21:28:02 -0500

    Modifications.

--- main.asm
+++ main.asm
@@ -1,50 +1,24 @@
 ; Bootloader that prints "Welcome to BubbleOS."
+org 0x7c00 ; Offset to absolute address calculations. This is required since the BIOS saves this code at memory location 0x7c00
 
-mov ah, 0x0e ; Scrolling teletype BIOS routine
+welcome_string: db 'Welcome to BubbleOS!', 0 ; Declare a null-terminating string
 
-mov al, 'W'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'c'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'm'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 't'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'B'
-int 0x10
-mov al, 'u'
-int 0x10
-mov al, 'b'
-int 0x10
-mov al, 'b'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'O'
-int 0x10
-mov al, 'S'
-int 0x10
-mov al, '!'
-int 0x10
+call print_string ; call == jump + push address to stack
 
-jmp $
+do_nothing_forever:
+	jmp $ ; Jump to current address (infinite jump)
+
+print_string:
+    mov al, [si] ; al = character to print
+	mov ah, 0x0e ; Scrolling teletype BIOS routine (when int (interrupt) = 0x10)
+	print_string_loop:
+		int 0x10 ; interrupt 0x10 for printing
+		inc si ; increment to next character
+		mov al, [si]
+		cmp al, 0 ; Compare character with null value
+		jne print_string_loop ; Jump if character is not equal to null
+		ret ; ret == remove address from stack + jump to it
+    ret
 
 times 510-($-$$) db 0 ; Pad binary file with zeroes to reach 510 bytes
-
 dw 0xAA55 ; The last 2 bytes are the magic number for the BIOS to interpret this sector as a boot sector
